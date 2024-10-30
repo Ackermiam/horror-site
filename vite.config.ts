623a8ab1a6483b37906b357dc72f82b4e66e288a
unknown <67873431+Ackermiam@users.noreply.github.com>
2024-10-31 00:07:14 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/horror-site',
   plugins: [vue()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendors': ['vue'],
+          'three-vendors': ['three'],
+        },
+      },
+    },
+  },
+});
